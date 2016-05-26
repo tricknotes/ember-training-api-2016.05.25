2c8691f3796ad7bc59076239fefa74a942bd9bcf
Ryunosuke Sato <tricknotes.rs@gmail.com>
2016-05-27 05:58:41 +0900

    Route for Ember App

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
 
+  get 'notes(/*any)' => 'welcome#index' # This is a endpoint for Ember App.
+
   namespace :api do
     resources :notes
   end
